71e31ef5a74176fa9d159df04bcbb33f23c7d5d8
centaure <centaure@gmail.com>
2018-02-25 23:33:26 -0800

    Test React frontend app (typescript):
    - Used starter example in [1].
    
    [1] https://github.com/Microsoft/TypeScript-React-Starter

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -6,7 +6,7 @@ const CopyWebpackPlugin        = require('copy-webpack-plugin');
 const DefinePlugin             = require('webpack/lib/DefinePlugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'javascripts');
-var APP_DIR = path.resolve(__dirname, '../app/assets/app');
+var APP_DIR = path.resolve(__dirname, './src');
 
 const ENV  = process.env.NODE_ENV = 'development';
 const HOST = process.env.HOST || 'localhost';
@@ -30,7 +30,7 @@ var config = {
   entry: {
     main : [
       'webpack-dev-server/client?https://localhost:8080',
-      APP_DIR + '/index.js'
+      APP_DIR + '/index.tsx'
     ]
   },
   output: {
@@ -41,28 +41,61 @@ var config = {
   },
   module : {
     loaders : [
-      { test : /\.jsx?/, include : APP_DIR, loader : 'babel-loader' },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "raw-loader"
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              includePaths: [
+                "./node_modules/bootstrap-sass/assets/stylesheets",
+                "../app/assets/app/stylesheets",
+                "./stylesheets"
+              ]
+            }
+          }
+        ]
+      },	  
+      // All files with a '.ts' or '.tsx' extension will be handled by
+      // 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      // All output '.js' files will have any sourcemaps re-processed
+      // by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
       { test: /\.json$/, loader: 'json-loader' },
       { test: /\.css$/,
-        loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]'
+        loader: 'style-loader!css-loader?localIdentName=[name]'
       },
       { test: /\.html$/,  loader: 'html-loader?caseSensitive=true' },
       { test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url-loader' }
     ]
   },
+  // &localIdentName=[name]__[local]___[hash:base64:5]
   plugins: [
 //    new CommonsChunkPlugin({name: 'vendor', filename: 'vendor.bundle.js', minChunks: Infinity}),
     new DefinePlugin({'webpack': {'ENV': JSON.stringify(metadata.ENV)}})
   ],
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".ts", ".tsx", ".js", ".json"],
     alias: {
       app: APP_DIR
     },
     modules: [
       path.join(__dirname, "node_modules")
     ]
-  }
+  },
+  // When importing a module whose path matches one of the following,
+  // just assume a corresponding global variable exists and use that
+  // instead. This is important because it allows us to avoid bundling
+  // all of our dependencies, which allows browsers to cache those
+  // libraries between builds.
+ /* externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  }, */
 };
 
 module.exports = config;
